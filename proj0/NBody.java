f12b5c46e91f4c4ba671b16e57d8cce3ac32164f
Ze Han <jh@ZeMachine.local>
2022-12-27 20:35:00 -0600

    commit

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -22,6 +22,46 @@ public class NBody {
             planetlist[i] = new Planet(xp,yp,xv,yv,m,imgpath);
         }
         return planetlist;
-    
+        
+    }
+    public static void main(String arg[]){
+        double T = Double.parseDouble(arg[0]);
+        double dt = Double.parseDouble(arg[1]);
+        String fileName = arg[2];
+        Planet[] planetList = NBody.readPlanets(fileName);
+        double radiusOfUniverse = NBody.readRadius(fileName);
+        StdDraw.setScale(-1,1);
+        StdDraw.picture(0,0,"images/starfield.jpg");
+        for (int i = 0; i< planetList.length; i ++){
+            planetList[i].draw(radiusOfUniverse);
+        }
+		StdDraw.enableDoubleBuffering();
+        double t_=0;
+        double[] xForce = new double[planetList.length];
+        double[] yForce = new double[planetList.length];
+        while (t_<T) {
+            StdDraw.clear();
+            for (int i = 0; i < planetList.length;i++){
+                xForce[i] = planetList[i].calcNetForceExertedByX(planetList);
+                yForce[i] = planetList[i].calcNetForceExertedByY(planetList);        
+            }
+            for (int i = 0; i < planetList.length;i++){
+                planetList[i].update(dt, xForce[i], yForce[i]);
+            }
+            StdDraw.picture(0,0,"images/starfield.jpg");
+            for (int i = 0; i < planetList.length;i++){
+                planetList[i].draw(radiusOfUniverse);
+            }            
+            StdDraw.show();
+            StdDraw.pause(10);
+            t_ = t_ + dt;
+        }
+        StdOut.printf("%d\n", planetList.length);
+        StdOut.printf("%.2e\n", radiusOfUniverse);
+        for (int i = 0; i < planetList.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+            planetList[i].xxPos, planetList[i].yyPos, planetList[i].xxVel,
+            planetList[i].yyVel, planetList[i].mass, planetList[i].imgFileName);   
+    }  
     }
 }
